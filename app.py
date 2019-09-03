7d4a1e4b7a3fe520faabf489452c81055efb78fb
Jasir <jasirfetai@gmail.com>
2019-09-03 16:12:25 +0200

    Migrate to Firestore

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from modules.firebase import readNews, readNewsByCategoryV2
+from modules.firestore import getNews
 from flask_cors import CORS
 
 
@@ -7,7 +7,6 @@ API_KEY = "f3761fb8-cd8c-4e1b-be62-382a3f654513"
 
 app = Flask(__name__)
 CORS(app)
-
 
 def toInt(tmpVal, default=0):
     if tmpVal is None or not tmpVal.isdigit:
@@ -18,23 +17,22 @@ def toInt(tmpVal, default=0):
 @app.route("/", methods=['GET'])
 def news_all():
     api_key = request.args.get("api_key")
-    limit = int(request.args.get('offset'))
-    startAt = int(request.args.get("startAt"))
+    limit = toInt(request.args.get('limit'), 10)
+    offset = toInt(request.args.get("offset"), 0)
     if api_key != API_KEY:
         return jsonify({"message": "Bad API Key!"})
-
-    news = readNews("portal", limit)[startAt:]
+    startAt = limit*offset
+    news = getNews(limit*(offset+1))[startAt:]
     return jsonify(total=len(news), news=news)
 
 
 @app.route("/<category>")
 def news_category(category):
     api_key = request.args.get("api_key")
-    limit = int(request.args.get('offset'))
-    startAt = int(request.args.get("startAt"))
-
+    limit = toInt(request.args.get('limit'), 10)
+    offset = toInt(request.args.get("offset"), 0)
     if api_key != API_KEY:
         return jsonify({"message": "Bad API Key!"})
-
-    news = readNewsByCategoryV2("portal", limit, category)[startAt:]
+    startAt = limit*offset
+    news = getNews(limit*(offset+1), category)[startAt:]
     return jsonify(total=len(news), news=news)
---
+++ modules/firestore.py
@@ -0,0 +1,17 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate('./serviceaccount.json')
+default_app = firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+
+def getNews(limit, category=""):
+    news_ref = db.collection("portal")
+    sport_news = []
+    if category != "":
+        sport_news = news_ref.where('category', '==', category).order_by('published_at').limit(limit).stream()
+    else:
+        sport_news = news_ref.order_by('published_at').limit(limit).stream()
+    sport_news = [n.to_dict() for n in sport_news]
+    return sport_news
