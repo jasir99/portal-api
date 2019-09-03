b1de43ad5a21b71b6438b95f7a04bf7bd9bd5598
Jasir <jasirfetai@gmail.com>
2019-09-03 16:17:24 +0200

    Migrate to Firestore

--- modules/firestore.py
+++ modules/firestore.py
@@ -10,8 +10,8 @@ def getNews(limit, category=""):
     news_ref = db.collection("portal")
     sport_news = []
     if category != "":
-        sport_news = news_ref.where('category', '==', category).order_by('published_at' direction=firestore.Query.DESCENDING).limit(limit).stream()
+        sport_news = news_ref.where('category', '==', category).order_by('published_at', direction=firestore.Query.DESCENDING).limit(limit).stream()
     else:
-        sport_news = news_ref.order_by('published_at' direction=firestore.Query.DESCENDING).limit(limit).stream()
+        sport_news = news_ref.order_by('published_at', direction=firestore.Query.DESCENDING).limit(limit).stream()
     sport_news = [n.to_dict() for n in sport_news]
     return sport_news
